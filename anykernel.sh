433322615214ea384ede2ce3a377c5c2f91cb7a0
Shoiya Akari <akariondev@gmail.com>
2024-10-27 13:13:39 -0300

    ak3: inline kernel support
    thx ghazzor

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 properties() { '
 kernel.string=MoeKernel by @whyakari
 do.devicecheck=1
-do.modules=1
+do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
@@ -13,24 +13,22 @@ supported.versions=14.0
 supported.patchlevels=
 '; }
 
+# shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-dump_boot;
-write_boot;
-
-block=vendor_boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-reset_ak;
+## AnyKernel boot install
 split_boot;
+
 flash_boot;
